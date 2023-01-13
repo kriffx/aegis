7cefc7bc9e000f93b4443dc9d2f30b2b2b9b121f
Cleverson Sampaio <cleverson_sampaio@outlook.com.br>
2023-01-13 19:33:33 -0300

    add validation in screenshot component

--- src/Aegis/gherkin/screenshot.py
+++ src/Aegis/gherkin/screenshot.py
@@ -1,6 +1,6 @@
 from typing import Union
 from SeleniumLibrary.base import LibraryComponent, keyword
-from SeleniumLibrary.keywords import ScreenshotKeywords
+from SeleniumLibrary.keywords import ScreenshotKeywords, WaitingKeywords
 from selenium.webdriver.remote.webelement import WebElement
 
 DEFAULT_FILENAME_PAGE = "selenium-screenshot-{index}.png"
@@ -11,6 +11,7 @@ class Screenshot(LibraryComponent):
     def __init__(self, ctx):
         LibraryComponent.__init__(self, ctx)
         self.screenshot = ScreenshotKeywords(ctx)
+        self.waiting = WaitingKeywords(ctx)
     
     @keyword("I set screenshot directory ${path}")
     def set_screenshot_directory(self, path: Union[None, str]) -> str:
@@ -26,8 +27,10 @@ class Screenshot(LibraryComponent):
 
     @keyword("I capture element ${locator} screenshot image")
     def capture_element_screenshot_image(self, locator: Union[WebElement, None, str]) -> str:
+        self.waiting.wait_until_element_is_visible(locator=locator)
         return self.screenshot.capture_element_screenshot(locator=locator, filename=DEFAULT_FILENAME_ELEMENT)
     
     @keyword("I capture element ${locator} screenshot embed")
     def capture_element_screenshot_embed(self, locator: Union[WebElement, None, str]) -> str:
+        self.waiting.wait_until_element_is_visible(locator=locator)
         return self.screenshot.capture_element_screenshot(locator=locator, filename=EMBED)
